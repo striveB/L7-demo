6f97b2af0e9a654ce61eb6ea5d8b20bf544357d3
jun233s <1227491415@qq.com>
2023-07-30 10:40:28 +0800

    初始l7-demo

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { Scene } from "@antv/l7";
+import { GaodeMap } from "@antv/l7-maps";
+// 同样你也可以初始化一个 Mapbox 地图
+const scene = new Scene({
+  id: "map",
+  map: new GaodeMap({
+    pitch: 35.210526315789465,
+    style: "dark",
+    center: [104.288144, 31.239692],
+    zoom: 4.4,
+  }),
+});
+</script>
+
+<template>
+  <div id="map" style="width: 100%; height: 100%"></div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
